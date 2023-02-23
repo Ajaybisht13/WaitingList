439820a9b61b77e5a3f66fc49483a06776ddbb8a
Ajay Bisht <ajaybisht203@gmail.com>
2023-02-23 12:05:36 +0530

    Create addcodeinnodemodulereactnativefolder.js

---
+++ addcodeinnodemodulereactnativefolder.js
@@ -0,0 +1,21 @@
+// Deprecated Prop Types
+get ColorPropType(): $FlowFixMe {
+  console.warn('');
+  return require('deprecated-react-native-prop-types').ColorPropType;
+},
+
+get EdgeInsetsPropType(): $FlowFixMe {
+  console.warn('');
+  return require('deprecated-react-native-prop-types').EdgeInsetsPropType;
+},
+
+get PointPropType(): $FlowFixMe {
+  console.warn('');
+  return require('deprecated-react-native-prop-types').PointPropType;
+},
+
+get ViewPropTypes(): $FlowFixMe {
+  console.warn('');
+  return require('deprecated-react-native-prop-types').ViewPropTypes;
+},
+};
